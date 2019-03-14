f182da19e1ec91a1ce24dfaba280a8be00e8e233
elixirtrip <elixirtrip@gmail.com>
2019-03-14 21:31:10 +0300

    Formated

--- bars.py
+++ bars.py
@@ -11,18 +11,22 @@ def load_data(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         try:
             return json.load(file)['features']
-        except json.decoder.JSONDecoderError:
+        except json.decoder.JSONDecodeError:
             return None
 
 
 def get_biggest_bar(bars_data):
-    return max(bars_data, key=lambda x: x['properties']['Attributes']
-               ['SeatsCount'])
+    return max(
+        bars_data,
+        key=lambda x: x['properties']['Attributes']['SeatsCount']
+    )
 
 
 def get_smallest_bar(bars_data):
-    return min(bars_data, key=lambda x: x['properties']['Attributes']
-               ['SeatsCount'])
+    return min(
+        bars_data,
+        key=lambda x: x['properties']['Attributes']['SeatsCount']
+    )
 
 
 def get_bar_longitude_latitude_coordinates(bar_data):
@@ -30,29 +34,40 @@ def get_bar_longitude_latitude_coordinates(bar_data):
 
 
 def get_closest_bar(bars_data, longitude, latitude):
-    return min(bars_data, key=lambda x:
-               math.hypot((get_bar_longitude_latitude_coordinates(x)[0]
-                           - longitude),
-                          (get_bar_longitude_latitude_coordinates(x)[1]
-                           - latitude))
-               )
+    return min(
+        bars_data,
+        key=lambda x: math.hypot(
+            (get_bar_longitude_latitude_coordinates(x)[0] - longitude),
+            (get_bar_longitude_latitude_coordinates(x)[1] - latitude)
+        )
+    )
 
 
 def print_formatted_bar_name(message, bar_data):
-    print('{0}\n{1}:\n{2}'.format(('-'*30), message, bar_data
-                                  ['properties']['Attributes']['Name']))
+    print(
+        '{0}\n{1}:\n{2}'.format(
+            ('-'*30),
+            message,
+            bar_data['properties']['Attributes']['Name']
+        )
+    )
 
 
 def get_console_args():
-    parser = argparse.ArgumentParser(prog='Biggest, smallest, closest Bars',
-                                     description='Count the closest, biggest'
-                                     'and smallest bar from the json file.')
+    parser = argparse.ArgumentParser(
+        prog='Biggest, smallest, closest Bars',
+        description='Count the closest, biggest'
+        'and smallest bar from the json file.'
+    )
 
-    parser.add_argument('--file_path', default='bars.json',
-                        help="If you haven't got file bars.jason in the script"
-                        "directory, specify the path for the file, or download"
-                        "it from link:"
-                        "https://devman.org/fshare/1503831681/4/")
+    parser.add_argument(
+        '--file_path',
+        default='bars.json',
+        help='If you havent got file bars.jason in the script'
+        'directory, specify the path for the file, or download'
+        'it from link:'
+        'https://devman.org/fshare/1503831681/4/'
+    )
 
     return parser.parse_args()
 
@@ -79,10 +94,18 @@ if __name__ == '__main__':
         sys.exit('Введено некорректное значение')
 
     print_formatted_bar_name(
-        'Самый большой бар', get_biggest_bar(bars_data))
-    print_formatted_bar_name('Самый маленький бар',
-                             get_smallest_bar(bars_data))
-    print_formatted_bar_name('Самый близкий бар',
-                             get_closest_bar(bars_data,
-                                             latitude_longitude_list[0],
-                                             latitude_longitude_list[1]))
+        'Самый большой бар',
+        get_biggest_bar(bars_data)
+    )
+    print_formatted_bar_name(
+        'Самый маленький бар',
+        get_smallest_bar(bars_data)
+    )
+    print_formatted_bar_name(
+        'Самый близкий бар',
+        get_closest_bar(
+            bars_data,
+            latitude_longitude_list[0],
+            latitude_longitude_list[1]
+        )
+    )
