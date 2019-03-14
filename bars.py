63cd802a7c7b1e115d6ce911ae3741c43fb26d0b
elixirtrip <elixirtrip@gmail.com>
2019-03-14 12:13:17 +0300

    wally erorr corection

--- bars.py
+++ bars.py
@@ -2,15 +2,14 @@ import json
 import math
 import argparse
 import sys
+import os
 
 
 def load_data(filepath):
+    if not os.path.exists(filepath):
+        return None
     with open(filepath, 'r', encoding='utf-8') as read_file:
-        try:
-            data = json.load(read_file)
-            return data['features']
-        except (FileNotFoundError, json.decoder.JSONDecodeError):
-            return None
+        return json.load(read_file)['features']
 
 
 def get_biggest_bar(data):
@@ -23,18 +22,18 @@ def get_smallest_bar(data):
                ['SeatsCount'])
 
 
-def get_bar_longitude_latitude(bar):
+def get_bar_longitude_latitude_coordinates(bar):
     return bar['geometry']['coordinates']
 
 
 def get_closest_bar(data, longitude, latitude):
     return min(data, key=lambda x:
-               math.hypot((get_bar_longitude_latitude(x)[0] - longitude),
-                          (get_bar_longitude_latitude(x)[1] - latitude))
+               math.hypot((get_bar_longitude_latitude_coordinates(x)[0] - longitude),
+                          (get_bar_longitude_latitude_coordinates(x)[1] - latitude))
                )
 
 
-def print_formatted_result(masseg, data):
+def print_formatted_bar_name(masseg, data):
     print('{0}\n{1}:\n{2}'.format(('-'*30), masseg, data
                                   ['properties']['Attributes']['Name']))
 
@@ -53,21 +52,30 @@ def get_console_args():
     return parser.parse_args()
 
 
-if __name__ == '__main__':
-    args = get_console_args()
+def input_your_latitude_longitude():
     try:
-        data = load_data(args.file_path)
-    except (FileNotFoundError, json.decoder.JSONDecodeError):
-        sys.exit('Файл не найден или данные не в формате JASON')
-    except Exception as e:
-        sys.exit(e)
-    try:
-        latitude = float(input('Введите широту Вашего местоположения:\n'))
-        longitude = float(input('Введите долготу Вашего местоположения:\n'))
+        latitude_longitude = [0.0, 0.0]
+        latitude_longitude[0] = float(
+            input('Введите широту Вашего местоположения:\n'))
+        latitude_longitude[1] = float(
+            input('Введите долготу Вашего местоположения:\n'))
+        return latitude_longitude
     except ValueError:
+        return None
+
+
+if __name__ == '__main__':
+    parse_args = get_console_args()
+    json_data = load_data(parse_args.file_path)
+    if json_data is None:
+        sys.exit('Файл не найден или данные не в формате JSON')
+    latitude_longitude = input_your_latitude_longitude()
+    if latitude_longitude is None:
         sys.exit('Введено некорректное значение')
 
-    print_formatted_result('Самый большой бар', get_biggest_bar(data))
-    print_formatted_result('Самый маленький бар', get_smallest_bar(data))
-    print_formatted_result('Самый близкий бар',
-                           get_closest_bar(data, longitude, latitude))
+    print_formatted_bar_name('Самый большой бар', get_biggest_bar(json_data))
+    print_formatted_bar_name('Самый маленький бар',
+                             get_smallest_bar(json_data))
+    print_formatted_bar_name('Самый близкий бар',
+                             get_closest_bar(json_data, latitude_longitude[0],
+                                             latitude_longitude[1]))
