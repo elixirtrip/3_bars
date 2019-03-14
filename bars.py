16ca379c985f697b9429d2fe4b915aff479e005d
elixirtrip <elixirtrip@gmail.com>
2019-03-14 17:13:38 +0300

    Change some variable names

--- bars.py
+++ bars.py
@@ -5,35 +5,37 @@ import sys
 import os
 
 
-def load_data(filepath):
-    if not os.path.exists(filepath):
+def load_data(file_path):
+    if not os.path.exists(file_path):
         return None
-    with open(filepath, 'r', encoding='utf-8') as read_file:
+    with open(file_path, 'r', encoding='utf-8') as read_file:
         return json.load(read_file)['features']
 
 
-def get_biggest_bar(data):
-    return max(data, key=lambda x: x['properties']['Attributes']
+def get_biggest_bar(json_file_data):
+    return max(json_file_data, key=lambda x: x['properties']['Attributes']
                ['SeatsCount'])
 
 
-def get_smallest_bar(data):
-    return min(data, key=lambda x: x['properties']['Attributes']
+def get_smallest_bar(json_file_data):
+    return min(json_file_data, key=lambda x: x['properties']['Attributes']
                ['SeatsCount'])
 
 
-def get_bar_longitude_latitude_coordinates(bar):
-    return bar['geometry']['coordinates']
+def get_bar_longitude_latitude_coordinates(bar_data):
+    return bar_data['geometry']['coordinates']
 
 
-def get_closest_bar(data, longitude, latitude):
-    return min(data, key=lambda x:
-               math.hypot((get_bar_longitude_latitude_coordinates(x)[0] - longitude),
-                          (get_bar_longitude_latitude_coordinates(x)[1] - latitude))
+def get_closest_bar(json_file_data, longitude, latitude):
+    return min(json_file_data, key=lambda x:
+               math.hypot((get_bar_longitude_latitude_coordinates(x)[0]
+                           - longitude),
+                          (get_bar_longitude_latitude_coordinates(x)[1]
+                           - latitude))
                )
 
 
-def print_formatted_bar_name(masseg, data):
+def print_formatted_bar_name(masseg, bar_data):
     print('{0}\n{1}:\n{2}'.format(('-'*30), masseg, data
                                   ['properties']['Attributes']['Name']))
 
@@ -54,28 +56,30 @@ def get_console_args():
 
 def input_your_latitude_longitude():
     try:
-        latitude_longitude = [0.0, 0.0]
-        latitude_longitude[0] = float(
+        latitude_longitude_list = [0.0, 0.0]
+        latitude_longitude_list[0] = float(
             input('Введите широту Вашего местоположения:\n'))
-        latitude_longitude[1] = float(
+        latitude_longitude_list[1] = float(
             input('Введите долготу Вашего местоположения:\n'))
-        return latitude_longitude
+        return latitude_longitude_list
     except ValueError:
         return None
 
 
 if __name__ == '__main__':
     parse_args = get_console_args()
-    json_data = load_data(parse_args.file_path)
-    if json_data is None:
+    json_file_data = load_data(parse_args.file_path)
+    if json_file_data is None:
         sys.exit('Файл не найден или данные не в формате JSON')
-    latitude_longitude = input_your_latitude_longitude()
-    if latitude_longitude is None:
+    latitude_longitude_list = input_your_latitude_longitude()
+    if latitude_longitude_list is None:
         sys.exit('Введено некорректное значение')
 
-    print_formatted_bar_name('Самый большой бар', get_biggest_bar(json_data))
+    print_formatted_bar_name(
+        'Самый большой бар', get_biggest_bar(json_file_data))
     print_formatted_bar_name('Самый маленький бар',
-                             get_smallest_bar(json_data))
+                             get_smallest_bar(json_file_data))
     print_formatted_bar_name('Самый близкий бар',
-                             get_closest_bar(json_data, latitude_longitude[0],
-                                             latitude_longitude[1]))
+                             get_closest_bar(json_file_data,
+                                             latitude_longitude_list[0],
+                                             latitude_longitude_list[1]))
